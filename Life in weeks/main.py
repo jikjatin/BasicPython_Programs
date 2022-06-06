8b7ffccf82fa41d9685fd064a7c62e2de6c59fb0
Jatin <91331734+jikjatin@users.noreply.github.com>
2022-06-06 23:16:51 +0530

    Create main.py

---
+++ Life in weeks/main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+int_age=90-int(age)
+days=int_age*365
+weeks=int_age*52
+months=int_age*12
+print(f"You have {days} days,{weeks} weeks,{months} months left.")
