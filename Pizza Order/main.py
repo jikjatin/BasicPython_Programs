c8347ef2f6553191d3f36e552ba3c6c927a5cb22
Jatin <91331734+jikjatin@users.noreply.github.com>
2022-08-28 14:39:19 +0530

    Update main.py

--- Pizza Order/main.py
+++ Pizza Order/main.py
@@ -10,19 +10,15 @@ if size=='S':
   bill=15
   if add_pepperoni=='Y':
     bill+=2
-  if extra_cheese=='Y':
-    bill+=1
 elif size=='M':
   bill=20
   if add_pepperoni=='Y':
     bill+=3
-  if extra_cheese=='Y':
-    bill+=1
 else:
   bill=25
   if add_pepperoni=='Y':
     bill+=3
-  if extra_cheese=='Y':
+if extra_cheese=='Y':
     bill+=1
 print(f"Your final bill is: ${bill}")    
 
